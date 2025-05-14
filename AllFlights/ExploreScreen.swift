4fd89368547225a304278418a7abcff58553691b
Swalih60 <swalihzamnoon60@gmail.com>
2025-05-14 16:27:56 +0530

    first commit

---
+++ AllFlights/ExploreScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreScreen.swift
+//  AllFlights
+//
+//  Created by Swalih Zamnoon P M on 14/05/25.
+//
+
+import SwiftUI
+
+struct ExploreScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreScreen()
+}
